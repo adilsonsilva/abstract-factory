9de3b205c0ed0a074242fe16ee011962f2c89ba8
Adilson Rodrigues da Silva <adilsons>
2020-11-19 23:21:27 -0300

    Padrão Projeto com abstract factory - 2

--- src/main/java/br/com/ars/padraoprojeto/abstractfactory/tvfactory/TVFactory.java
+++
@@ -1,9 +0,0 @@
-package br.com.ars.padraoprojeto.abstractfactory.tvfactory;
-
-import br.com.ars.padraoprojeto.abstractfactory.tv.TV;
-
-public interface TVFactory {
-    
-    public TV createTV();
-
-}
